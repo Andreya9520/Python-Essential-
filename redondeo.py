eb9e648402a5caf3caf9bb56435006961660088f
Andreya9520 <86445242+Andreya9520@users.noreply.github.com>
2021-06-25 14:35:58 -0500

    Add files via upload

---
+++ redondeo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 25 14:35:31 2021
+
+@author: andreavergara
+"""
+
+"""
+Estructura para redondeo 
+"""
+pi=22/7
+print (pi) 
+print("El valor de pi es: ", pi)
+
+print('{:.4f}'.format(pi))
+print('{:.8f}'.format(pi))
+print('{:.10f}'.format(pi)) #El numero maximo de decimales es 51 
